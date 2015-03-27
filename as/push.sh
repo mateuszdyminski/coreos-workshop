1d3d43533a46660ac405e3a141bd8c278d46a4e6
Mateusz Dymiński <dyminski@gmail.com>
2015-03-27 13:11:22 +0100

    v2

--- as/push.sh
+++ as/push.sh
@@ -2,7 +2,7 @@
 
 echo "Pushing AS container started..."
 set -e
-docker build -t quay.io/mateuszdyminski/as:latest "."
-docker push quay.io/mateuszdyminski/as
+docker build -t mateuszdyminski/as:latest "."
+docker push mateuszdyminski/as
 set +e
 echo "AS pushed to quay.io!"
--- lb/push.sh
+++ lb/push.sh
@@ -2,7 +2,7 @@
 
 echo "Pushing AS-LB container started..."
 set -e
-docker build -t quay.io/mateuszdyminski/as-lb:latest "."
-docker push quay.io/mateuszdyminski/as-lb
+docker build -t mateuszdyminski/as-lb:latest "."
+docker push mateuszdyminski/as-lb
 set +e
 echo "AS-LB pushed to quay.io!"
